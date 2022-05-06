ef74e844082f3f1ef456df53664b27d9caa3870a
TanzimTayef <tayef464@gmail.com>
2022-05-06 09:02:37 +0600

    added spacefic a id product

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // PORT
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -14,44 +14,49 @@ app.use(express.json());
 
 // connect with mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vdf6i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 async function run() {
+  try {
+    await client.connect();
+    const stockCollection = client.db("bikeWarehouse").collection("stocks");
 
-    try {
-        await client.connect();
-        const stockCollection = client.db('bikeWarehouse').collection('stocks');
+ // single item  
+    app.get("/stocks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const stock = await stockCollection.findOne(query);
+      res.send(stock);
+    });
 
-           
-            // load all data 
-            app.get("/stocks", async (req, res) => {
-                const query = {};
-                const cursor = stockCollection.find(query);
-                const stocks = await cursor.toArray();
-                res.send(stocks);
-            });
+    // load all data
+    app.get("/stocks", async (req, res) => {
+      const query = {};
+      const cursor = stockCollection.find(query);
+      const stocks = await cursor.toArray();
+      res.send(stocks);
+    });
 
-          // POST (add data)
-          app.post("/stocks", async (req, res) => {
-            const newStock = req.body;
-            const result = await stockCollection.insertOne(newStock);
-            res.send(result);
-        })
-
-
-    } finally {
-        
-    }
+    // POST (add data)
+    app.post("/stocks", async (req, res) => {
+      const newStock = req.body;
+      const result = await stockCollection.insertOne(newStock);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
-    res.send("Bike warehouse running");
+  res.send("Bike warehouse running");
 });
 
 // server running
 app.listen(PORT, () => {
-    console.log("Bike warehouse is running", PORT);
+  console.log("Bike warehouse is running", PORT);
 });
